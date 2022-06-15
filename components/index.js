6d0501677a4639331a11a3866bad1c22e0dd4535
Manuchimso Oliver <manuchimoliver779@gmail.com>
2022-06-15 16:51:45 +0100

    Initial commit 2

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTcard from "./NFTcard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+
+export {
+    HomeHeader, FocusedStatusBar, NFTcard, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo
+}
